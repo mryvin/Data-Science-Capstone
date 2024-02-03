3837308b742f3314d485bf505b3d6d151dfca9e0
mryvin <89282726+mryvin@users.noreply.github.com>
2024-02-03 18:58:19 -0500

    Update 2 Data Exploration.R

--- 2 Data Exploration.R
+++ 2 Data Exploration.R
@@ -1,40 +1,47 @@
-install.packages("BSDA")
 library(BSDA)
 
-setwd("/Users/mryvi/Documents/Data Capstone")
+setwd()
 
+# Read the cleaned Airbnb dataset from a CSV file
 airbnb <- read.csv("Cleaned_Airbnb.csv")
 
-airbnb
-
+# Create subsets for each neighborhood group: Queens, Brooklyn, Bronx, Manhattan
 queens = subset(airbnb, neighbourhood.group == 'Queens')
 brooklyn = subset(airbnb, neighbourhood.group == 'Brooklyn')
 bronx = subset(airbnb, neighbourhood.group == 'Bronx')
 manhattan = subset(airbnb, neighbourhood.group == 'Manhattan')
 
+########################################################################################### 
+# Proportion Z-test of Neighbourhood group vs floor
+########################################################################################### 
 
-## Proportion Z-test of Neighbourhood group vs floor
+# Display the frequency table of the overall neighborhood groups
 table(airbnb$neighbourhood.group)
+
+# Display the frequency table of floors in Queens
 table(queens$floor)
+
+# Display the frequency table of floors in Brooklyn
 table(brooklyn$floor)
 
-4530/4533
-
-4/16084
-
-prop.test(c(4530,4), c(4533,16084), p = NULL, alternative = "greater",
+# Proportion Z-test comparing the proportion of floors in Queens to the overall dataset
+prop.test(c(4530, 4), c(4533, 16084), p = NULL, alternative = "greater",
           correct = TRUE)
 
+########################################################################################### 
+# Sample Z-test comparing the average noise levels between Manhattan and Bronx
+########################################################################################### 
 
-#Sample Z-test of average noise vs neighbourhood group
-
+# Calculate mean and standard deviation of noise levels in Manhattan
 mean(manhattan$noise.dB.)
 sd(manhattan$noise.dB.)
 
+# Calculate mean and standard deviation of noise levels in Bronx
 mean(bronx$noise.dB.)
 sd(bronx$noise.dB.)
 
-z.test(manhattan$noise.dB.,bronx$noise.dB.,
+# Perform a Z-test comparing noise levels in Manhattan and Bronx
+z.test(manhattan$noise.dB., bronx$noise.dB.,
        alternative = "less",
        sigma.x = sd(manhattan$noise.dB.),
        sigma.y = sd(bronx$noise.dB.),
